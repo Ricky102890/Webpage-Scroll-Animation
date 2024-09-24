b2d983b20418d638a90ab2d54750186d470fbcd3
CJAX.Dev <jackson102890@gmail.com>
2024-09-24 14:19:51 -0600

    add scroll css and javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Variables
-
+const sections = document.querySelectorAll("section");
+const currentHeight = document.querySelector(".current-height");
+const current = document.querySelector(".current");
 // Functions
 
 // Events
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let secOffset = sec.offsetTop - 200;
+        let secHeight = sec.offsetHeight;
+
+        if (top < secOffset + secHeight) {
+            sec.classList.add("show-animate");
+
+        }
+        else {
+            sec.classList.remove("show-animate");
+        }
+        currentHeight.innerHTML = `Current Window Height: ${window.scrollY}, Div Height: ${secHeight}, Div OffsetHeight: ${secOffset}`;
+
+    })
+}
